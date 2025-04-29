0909c7553f5d3465ad6980db7f63eb3770b22db9
MM230159 <124802181+maldonado20@users.noreply.github.com>
2025-04-29 11:16:11 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,10 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(marketRoutes)
 
-app.use((req,res,next)=>{
-    res.status(404).json({
-        message:'Endpoint No encontrado'
-    })
-})
-
+app.use((req, res, next) => {
+ res.status(404).json({
+ message: "Favor realizar pruebas en los siguientes endpoints:",
+ endpoints: [
+ "https://URL GENERADA Railway /usuarios",
+ "https:// URL GENERADA Railway /productos"
+ ]
+ });
+});
 export default app;
