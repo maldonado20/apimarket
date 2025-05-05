517dc551c7a30aa5588c42f0a90942403250e366
MM230159 <124802181+maldonado20@users.noreply.github.com>
2025-05-05 11:54:01 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
-import express from 'express'
+import express from 'express';
 import marketRoutes from './routes/market.routes.js'
-
 const app=express()
-
 // Middleware para procesar JSON
 app.use(express.json());
-
-// Middleware para procesar datos de formularios (opcional, si usas x-www-form-urlencoded)
+// Middleware para procesar datos de formularios (opcional, si usas x-www-formurlencoded)
 app.use(express.urlencoded({ extended: true }));
-
 app.use(marketRoutes)
-
 app.use((req, res, next) => {
  res.status(404).json({
  message: "Favor realizar pruebas en los siguientes endpoints:",
