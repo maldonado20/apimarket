bc6cc333fc6422fc0619fcb03af5fcdcdd9b402b
MM230159 <124802181+maldonado20@users.noreply.github.com>
2025-05-05 09:44:58 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ export const PORT= process.env.PORT || 3306
 export const DB_HOST=process.env.DB_HOST || 'mysql.railway.internal'
 export const DB_PORT=process.env.DB_PORT || 3306
 export const DB_USER=process.env.DB_USER || 'root'
-export const DB_PASSWORD=process.env.DB_PASSWORD || 'vmFRSbuqiSymlmvRaAKREZPMKTuxclD'
+export const DB_PASSWORD=process.env.DB_PASSWORD || 'vnFRSbuqiSymlmvRaAkREZPRMkTuxclD'
 export const DB_DATABASE=process.env.DB_DATABASE || 'railway'
