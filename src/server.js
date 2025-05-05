abde4927eed6a83d91752d91f78846f14eda1d76
MM230159 <124802181+maldonado20@users.noreply.github.com>
2025-05-05 11:51:56 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import app from './app.js';
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>{
-  console.log(`Servidor corriendo en el puerto ${PORT}`)
+app.listen(PORT, () => {
+ console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
